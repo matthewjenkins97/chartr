13ac927a59cfbb55fcc3814b9692b62d762fdc7c
Matthew R. Jenkins <matthewjenkins97@gmail.com>
2022-02-20 20:31:01 -0500

    implemented sorting feature as enhancement #17, needs testing before I make a pull request

--- src/MainView/AlbumCollageOptions.svelte
+++ src/MainView/AlbumCollageOptions.svelte
@@ -1,6 +1,7 @@
 <script>
   import { currentChartList, currentChartId } from "../store";
   import ChartOption from "../shared/ChartOption.svelte";
+  import Button from "../shared/Button.svelte";
 
   let currentIndex = "";
 
@@ -17,6 +18,72 @@
   $: {
     document.documentElement.style.setProperty("--item-zoom", `${zoomValue}`);
   }
+
+  //
+  // Sorting the collage
+  //
+
+  // Sort functions we feed into the algorithm
+  // Using title from image because it has both album and artist in a string
+  const artistSort = (album1, album2) => {
+    if (album1.title < album2.title) return -1;
+    if (album1.title > album2.title) return 1;
+    return 0;
+  }
+
+  // Using album
+  const albumSort = (album1, album2) => {
+    if (album1.album < album2.album) return -1;
+    if (album1.album > album2.album) return 1;
+    return 0;
+  }
+
+  // Main sorting function
+  //
+  // Known issue: sorting works with Album only if added from Chartr,
+  // If an album is provided by a hyperlink, the hyperlink is set as 
+  // the album title and it sorts by that rather than by the actual 
+  // album name. There also may be album name glitching because of this.
+  //
+  const sortCollage = (sortMethod) => {
+
+    // get collage list, flatten it, remove all undefined / nulls, sort it
+    let listToSort = $currentChartList[currentIndex].albumCollageList;
+    const rows = listToSort.length;
+    const columns = listToSort[0].length;
+
+    let listElements = listToSort.flat();
+    listElements = listElements.filter(album => album != undefined);
+    listElements.sort(sortMethod);
+
+    // make new array with same dimensions as original list
+    const rebuiltList = [];
+    for (let row_index = 0; row_index < rows; row_index++) {
+      let row = [];
+      for (let column_index = 0; column_index < columns; column_index++) {
+        let column = undefined;
+        row.push(column);
+      }
+      rebuiltList.push(row);
+    }
+
+    // go through each row / column and place sorted values
+    // in a spot with an already existing album art
+    let listElementsIndex = 0;
+    for (let row_index = 0; row_index < rows; row_index++) {
+      for (let column_index = 0; column_index < columns; column_index++) {
+        if (listToSort[row_index][column_index] != undefined) {
+          rebuiltList[row_index][column_index] = listElements[listElementsIndex];
+          listElementsIndex++;
+        }
+      }
+    }
+
+    // save to albumCollageList
+    $currentChartList[currentIndex].albumCollageList = rebuiltList;
+  };
+
+
 </script>
 
 <ChartOption
@@ -94,3 +161,17 @@
   min="0"
   bind:value={$currentChartList[currentIndex].albumCollageOptions.gap}
 />
+
+<div style={{width: "auto", margin: "auto"}}>
+  <Button
+    label="Sort by Album"
+    id='sortAlbum'
+    onClick={() => sortCollage(albumSort)}
+  />
+  <br />
+  <Button
+    label="Sort by Artist"
+    id='sortArtist'
+    onClick={() => sortCollage(artistSort)}
+  />
+</div>
